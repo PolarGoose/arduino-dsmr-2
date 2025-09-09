e5a6ee8ffa0f274e40076921389e0c5eb15205f2
PolarGoose <35307286+PolarGoose@users.noreply.github.com>
2025-09-09 20:21:21 +0200

    * Apply clang-format * Fix comment formatting: use "//" for all comments

--- src/test/parser_include_test.cpp
+++ src/test/parser_include_test.cpp
@@ -1,15 +1,14 @@
 // This code tests that the parser has all necessary dependencies included in its headers.
 // We check that the code compiles.
 
-#include "dsmr/parser.h"
 #include "dsmr/fields.h"
+#include "dsmr/parser.h"
 
 using namespace dsmr;
 using namespace fields;
 
 void SomeFunction() {
   const auto& msg = "";
-
-  ParsedData <identification, p1_version> data;
+  ParsedData<identification, p1_version> data;
   const auto& res = P1Parser::parse(&data, msg, std::size(msg), true);
 }
